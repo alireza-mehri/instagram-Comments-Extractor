83d640d9b92cbf93ea7087cc6c36c8524e994d01
Alireza mehri <36499843+alireza-mehri@users.noreply.github.com>
2021-03-03 16:19:47 +0330

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -4,7 +4,7 @@ from urllib.request import urlopen
 
 # parsing getcombot url  
 # get url
-html_page = urlopen("https://getcombot.com/report.php?code=1614681124&id=1314794&lang=en")
+html_page = urlopen("your getcombot.com Url")
 
 soup = BeautifulSoup(html_page, 'html.parser')
 # find table with "main" id in html page
